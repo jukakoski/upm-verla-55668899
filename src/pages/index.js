9b4f6d0a0c3c99ae7af0b5b66896f09a485027d8
Miika Jukakoski <miika.jukakoski@ourmind.io>
2022-03-23 14:56:56 +0200

    swiper

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,64 @@ import Intro from "../components/intro";
 import MoreStories from "../components/more-stories";
 import { HelmetDatoCms } from "gatsby-source-datocms";
 import { graphql } from "gatsby";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation, EffectCoverflow } from "swiper";
+
+import 'swiper/css';
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+
+import "../styles/swiperStyles.css"
 
 export default function Index({ data: { allPosts, site, blog } }) {
   const heroPost = allPosts.nodes[0];
   const morePosts = allPosts.nodes.slice(1);
 
+
+
+
+
+
+
+
   return (
     <Container>
       <HelmetDatoCms seo={blog.seo} favicon={site.favicon} />
       <Intro />
+      <Swiper
+        slidesPerView={"auto"}
+        effect={"coverflow"}
+        grabCursor={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        spaceBetween={50}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation, EffectCoverflow]}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="https://www.metsa.fi/wp-content/uploads/2020/03/Talvimoto_Jari_Salonen_800x450.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="https://www.mtk.fi/o/adaptive-media/image/5280705/preview-2000x0/Mets%C3%A4kuva.PNG" /></SwiperSlide>
+        <SwiperSlide><img src="https://www.euractiv.com/wp-content/uploads/sites/2/2017/02/Finland-forest-Helsinki.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="http://cen.acs.org/content/dam/cen/96/7/09607-scitech2-finlandcxd-700.jpg" /></SwiperSlide>
+      </Swiper>
+      <br></br>
+      <br></br>
       {heroPost && (
         <HeroPost
           title={heroPost.title}
